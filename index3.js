1465898bcfc85a731661d7f115a6dad41717add0
zin516366-cloud <zin516366@gmail.com>
2026-07-25 23:22:32 +0700

    Update index3.js

--- index3.js
+++ index3.js
@@ -218,16 +218,16 @@ class LotteryAPI {
             "Content-Type": "application/json;charset=UTF-8",
             "Origin": this.getOrigin(),
             "Referer": this.getReferer(),
-            "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 18_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/18.3 Mobile/22D63 Safari/604.1"
+            "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 18_7 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/18.7.5 Mobile/15E148 Safari/604.1"
         };
     }
 
     getOrigin() {
-        return "https://www.6win566.com";
+        return "https://cklottery.cc";
     }
 
         getReferer() {
-            return "https://www.6win566.com/";
+            return "https://cklottery.cc/";
     }
 
     signMd5(data) {
